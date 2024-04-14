ac3c4fa4164350553a154a3b2d7e527c228fdefe
asheroh <wnstj701@dongguk.edu>
2024-04-14 16:38:26 +0900

    ag: Solve 9506

---
+++ baekjoon/step9/9506.js
@@ -0,0 +1,44 @@
+// 문제
+// 어떤 숫자 n이 자신을 제외한 모든 약수들의 합과 같으면, 그 수를 완전수라고 한다.
+
+// 예를 들어 6은 6 = 1 + 2 + 3 으로 완전수이다.
+
+// n이 완전수인지 아닌지 판단해주는 프로그램을 작성하라.
+
+// 입력
+// 입력은 테스트 케이스마다 한 줄 간격으로 n이 주어진다. (2 < n < 100,000)
+
+// 입력의 마지막엔 -1이 주어진다.
+
+// 출력
+// 테스트케이스 마다 한줄에 하나씩 출력해야 한다.
+
+// n이 완전수라면, n을 n이 아닌 약수들의 합으로 나타내어 출력한다(예제 출력 참고).
+
+// 이때, 약수들은 오름차순으로 나열해야 한다.
+
+// n이 완전수가 아니라면 n is NOT perfect. 를 출력한다.
+
+let input = require("fs")
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+function solution(input) {
+  for (let i = 0; i < input.length - 1; i++) {
+    let divisor = [];
+    for (let j = 1; j <= input[i]; j++) {
+      if (input[i] % j === 0) divisor.push(j);
+    }
+    divisor.pop();
+    if (divisor.reduce((a, c) => a + c) === input[i]) {
+      console.log(`${input[i]} = ${divisor.join(" + ")}`);
+    } else {
+      console.log(`${input[i]} is NOT perfect.`);
+    }
+  }
+}
+
+solution(input);
