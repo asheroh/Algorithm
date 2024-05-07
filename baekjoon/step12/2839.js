2c640a564e1ad3148a92880f95c6dc61f087deb1
asheroh <wnstj701@dongguk.edu>
2024-05-07 19:39:09 +0900

    ag: Solve 2839 using dp

--- baekjoon/step12/2839.js
+++ baekjoon/step12/2839.js
@@ -1,21 +1,25 @@
 //
 let N = +require("fs").readFileSync(0).toString();
 
-let answer = 0;
+function solution(N) {
+  let dp = new Array(N + 1).fill(Infinity);
+  // dp 배열 초기화
+  dp[0] = 0;
 
-while (true) {
-  if (N % 5 === 0) {
-    answer += N / 5;
-    break;
+  // 각 무게에 대해 최소 봉지 개수 계산
+  for (let i = 3; i <= N; i++) {
+    // 3kg 봉지 사용
+    if (i - 3 >= 0) {
+      dp[i] = Math.min(dp[i], dp[i - 3] + 1);
+    }
+    // 5kg 봉지 사용
+    if (i - 5 >= 0) {
+      dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+    }
   }
 
-  N -= 3;
-  answer++;
-
-  if (N < 0) {
-    answer = -1;
-    break;
-  }
+  // Nkg를 만들 수 없는 경우 -1 반환, 아니면 최소 봉지 개수 반환
+  return dp[N] === Infinity ? -1 : dp[N];
 }
 
-console.log(answer);
+console.log(solution(N));
