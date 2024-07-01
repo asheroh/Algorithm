8bdc3467d550e2343417ac4b2fcc050428b1deb6
asheroh <wnstj701@dongguk.edu>
2024-07-01 18:21:45 +0900

    ag: Solve 1929 eratosthenes

---
+++ baekjoon/step15/1929.js
@@ -0,0 +1,17 @@
+//
+const [M, N] = require("fs").readFileSync(0).toString().split(" ").map(Number);
+
+let prime = Array.from({ length: N + 1 }, () => true);
+prime.fill(false, 0, 2);
+
+for (let i = 2; i <= Math.sqrt(N); i++) {
+  if (prime[i]) {
+    for (let j = i * i; j <= N; j += i) {
+      prime[j] = false;
+    }
+  }
+}
+
+for (let target = M; target <= N; target++) {
+  if (prime[target] === true) console.log(target);
+}
