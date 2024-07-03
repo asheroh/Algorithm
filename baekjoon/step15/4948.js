a4080b7472d74c10c1f525da0302b4a2c5f16392
asheroh <wnstj701@dongguk.edu>
2024-07-03 17:22:30 +0900

    ag: Solve 4948 with eratosthenes

--- baekjoon/step15/4948.js
+++ baekjoon/step15/4948.js
@@ -7,18 +7,37 @@ let [...testcases] = require("fs")
   .map(Number);
 testcases.pop();
 
-function isPrime(n) {
-  if (n < 2) return false;
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) return false;
-  }
-  return true;
-}
-
+// 에라토스테네스의 체
 for (const n of testcases) {
+  let prime = Array.from({ length: n * 2 + 1 }, () => true);
+  prime.fill(false, 0, 2);
+
+  for (let i = 2; i <= Math.sqrt(n * 2); i++) {
+    if (prime[i]) {
+      for (let j = i * i; j <= n * 2; j += i) {
+        prime[j] = false;
+      }
+    }
+  }
   let count = 0;
   for (let i = n + 1; i <= n * 2; i++) {
-    if (isPrime(i)) count++;
+    if (prime[i]) count++;
   }
   console.log(count);
 }
+
+// function isPrime(n) {
+//   if (n < 2) return false;
+//   for (let i = 2; i <= Math.sqrt(n); i++) {
+//     if (n % i === 0) return false;
+//   }
+//   return true;
+// }
+
+// for (const n of testcases) {
+//   let count = 0;
+//   for (let i = n + 1; i <= n * 2; i++) {
+//     if (isPrime(i)) count++;
+//   }
+//   console.log(count);
+// }
