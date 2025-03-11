d4ab85fa4e41d960adf2a0567d362d178ea12cbb
asheroh <wnstj701@dongguk.edu>
2025-03-11 13:18:47 +0900

    ag: Solve 2164 shortcut bitwise ref

--- baekjoon/step16/2164.js
+++ baekjoon/step16/2164.js
@@ -1,20 +1,6 @@
 //
 const N = +require("fs").readFileSync(0).toString().trim();
 
-let count = 0;
-const stack = Array.from({ length: N }, () => {
-  count++;
-  return count;
-});
+let power = 1 << Math.floor(Math.log2(N));
 
-let idx = 0;
-while (idx < stack.length) {
-  if (stack.length === idx) break;
-  idx++;
-  if (stack.length === idx) break;
-  const top = stack[idx];
-  stack.push(top);
-  idx++;
-}
-
-console.log(stack[stack.length - 1]);
+console.log(N === power ? N : (N - power) * 2);
